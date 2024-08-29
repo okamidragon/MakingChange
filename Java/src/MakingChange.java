f05cd9683ae88f39f9de6fa1abcf4bbda5104059
Maddock <143650411+okamidragon@users.noreply.github.com>
2024-08-29 11:55:48 -0500

    MakingChange.java

--- Java/src/MakingChange.java
+++ Java/src/MakingChange.java
@@ -2,12 +2,10 @@ import javax.swing.*;
 
 public class MakingChange {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Making Change");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(600, 400);
-            frame.add(new RegisterPanel());
-            frame.setVisible(true);
-        });
+        JFrame frame = new JFrame("Making Change");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 400);
+        frame.setContentPane(new RegisterPanel(new Register()));
+        frame.setVisible(true);
     }
 }
