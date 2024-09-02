cf65b8d129919dc4e0ef73e5d2dbe34cd5998fe4
Maddock <143650411+okamidragon@users.noreply.github.com>
2024-09-02 15:40:51 -0500

    Update RegisterPanel.java

--- Java/src/RegisterPanel.java
+++ Java/src/RegisterPanel.java
@@ -2,11 +2,13 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+// Main class for the register panel
 public class RegisterPanel extends JPanel {
     private final Register register = new Register();
     private final JTextField input = new JTextField(10);
     private final PursePanel changePanel = new PursePanel();
 
+    // Creates the register panel
     public RegisterPanel() {
         input.addActionListener(new InputListener());
         add(new JLabel("Enter Amount:"));
@@ -14,13 +16,17 @@ public class RegisterPanel extends JPanel {
         add(changePanel);
     }
 
+    // Takes the amount input by the user
     private class InputListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             try {
                 double amount = Double.parseDouble(input.getText());
                 Purse purse = register.makeChange(amount);
                 changePanel.setPurse(purse);
-            } catch (NumberFormatException ex) {
+            }
+
+            // Checks if the amount input is invalid
+            catch (NumberFormatException ex) {
                 JOptionPane.showMessageDialog(RegisterPanel.this, "Invalid amount");
             }
         }
