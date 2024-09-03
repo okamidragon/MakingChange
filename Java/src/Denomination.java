4b5fae0d589dd1136b81566a225d3c7938d9f4b1
Maddock <143650411+okamidragon@users.noreply.github.com>
2024-09-03 09:26:20 -0500

    Update Denomination.java

--- Java/src/Denomination.java
+++ Java/src/Denomination.java
@@ -1,2 +1,5 @@
+/* Maddock Davis
+Lab 1 - Making Change
+Takes an amount of money from the user and shows how man coins or bills makes it. */
 // Declares the Denomination record.
 public record Denomination(String name, double amt, String form, String img) {}
